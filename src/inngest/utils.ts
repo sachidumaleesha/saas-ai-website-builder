34ac7315baf122a0982bb4ad7aa77ca47c2c04a5
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2025-07-05 13:28:53 +0530

    06: e2b sandbox

---
+++ src/inngest/utils.ts
@@ -0,0 +1,8 @@
+// get sandbox
+
+import Sandbox from "@e2b/code-interpreter";
+
+export async function getSandbox(sandboxId: string) {
+  const sandbox = await Sandbox.connect(sandboxId);
+  return sandbox;
+};
