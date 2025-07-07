0d030a7cd28411ae97b3a374a5f4807d8616f7b7
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2025-07-07 22:29:12 +0530

    09: projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
+import { projectRouter } from "@/modules/projects/server/procedure";
 import { createTRPCRouter } from "../init";
 import { messageRouter } from "@/modules/messages/server/procedure";
 export const appRouter = createTRPCRouter({
-  messages: messageRouter
+  messages: messageRouter,
+  projects: projectRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
